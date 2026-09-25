f3e044a684703a8c20bef3b25e5653d41d032d53
Israel Oliveira <raloliver.ws@gmail.com>
2026-09-25 14:15:38 -0300

    Atom Button (#1)
    * add tailwind css v4
    
    * Atom Button
    
    * Atom Button: more examples
    
    * Atom Button: replace css by tailwind classes
    
    * Atom Button: remove hard-coded color and add a new @theme

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,16 @@
+/*
+ * File: app.config.ts
+ * Project: design-system
+ * Created: Friday, August 28th 2026, 9:20:44 am
+ * Last Modified: Friday, August 28th 2026, 9:45:19 am
+ * Copyright © 2026 AMDE Agência
+ */
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
+/*
+ * File: app.routes.ts
+ * Project: design-system
+ * Created: Friday, August 28th 2026, 9:20:44 am
+ * Last Modified: Friday, August 28th 2026, 9:52:18 am
+ * Copyright © 2026 AMDE Agência
+ */
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [];
